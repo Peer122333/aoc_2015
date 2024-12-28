d7ccb6cc28b7aff022abc7709e69f0d10fa1c6b9
peerlevi <orzelek@hm.edu>
2024-12-28 12:06:16 +0100

    Initial Commit

---
+++ AOC2015/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AOC2015
+//
+//  Created by Peer Orzelek on 28.12.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
